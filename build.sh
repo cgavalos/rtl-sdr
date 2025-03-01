2382c5d4462f1ac60d0a1b64679ea56791daeae1
Carlos Gilberto Avalos <carlos.g.avalos@gmail.com>
2025-02-28 20:44:15 -0500

    Sudo all apt commands

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-sudo apt update && apt upgrade -y && apt install git cmake make libopencv-dev g++ libairspy-dev librtlsdr-dev rtl-sdr vim -y
+sudo apt update && sudo apt upgrade -y && sudo apt install git cmake make libopencv-dev g++ libairspy-dev librtlsdr-dev rtl-sdr vim -y
 git clone --recursive https://github.com/pietern/goestools
 cmake -S goestools -B goestools/build
 cmake --build goestools/build
